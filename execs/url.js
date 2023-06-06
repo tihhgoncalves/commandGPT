232539eeb9fcdfe2e9f1cb97b7f12eafbe309475
Tihh Gonçalves <tihhgoncalves@gmail.com>
2023-06-05 23:26:44 -0300

    melhorias no exec url

--- execs/url.js
+++ execs/url.js
@@ -1,11 +1,27 @@
 const axios = require('axios');
 
 module.exports = async function(url) {
-    try {
-        const response = await axios.get(url);
-        return response.data;
-    } catch (error) {
-        console.error(`Erro ao acessar a URL ${url}: ${error.message}`);
-        return 'erro';
+  let urlString;
+
+  if (typeof url === 'string') {
+    urlString = url;
+  } else if (typeof url === 'object' && url.url) {
+    urlString = url.url;
+  } else {
+    console.error(`URL inválida: ${url}`);
+    return 'erro';
+  }
+
+  try {
+    const response = await axios.get(urlString);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error(`Erro ao acessar a URL ${urlString}: Status ${response.status}`);
+      return 'erro';
     }
+  } catch (error) {
+    console.error(`Erro ao acessar a URL ${urlString}: ${error.message}`);
+    return 'erro';
+  }
 };
